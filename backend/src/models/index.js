ca7e83eec17aa90b0337b2cc5eaf77cba98cca8e
CCS <you@example.com>
2026-08-13 15:38:48 +0200

    fix models file clean version

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,6 +1,46 @@
 const sequelize = require('../config/db');
 const User = require('./User');
 const Customer = require('./Customer');
-git add .
-git commit -m "restore full postgres models"
-git push origin main
+const Vehicle = require('./Vehicle');
+const Booking = require('./Booking');
+const Service = require('./Service');
+const Inventory = require('./Inventory');
+const JobCard = require('./JobCard');
+const Employee = require('./Employee');
+const Invoice = require('./Invoice');
+const Message = require('./Message');
+
+User.hasMany(Customer, { foreignKey: 'createdBy' });
+Customer.belongsTo(User, { foreignKey: 'createdBy' });
+
+Customer.hasMany(Vehicle, { foreignKey: 'customerId' });
+Vehicle.belongsTo(Customer, { foreignKey: 'customerId' });
+
+Customer.hasMany(Booking, { foreignKey: 'customerId' });
+Booking.belongsTo(Customer, { foreignKey: 'customerId' });
+
+Vehicle.hasMany(Booking, { foreignKey: 'vehicleId' });
+Booking.belongsTo(Vehicle, { foreignKey: 'vehicleId' });
+
+Booking.hasOne(JobCard, { foreignKey: 'bookingId' });
+JobCard.belongsTo(Booking, { foreignKey: 'bookingId' });
+
+Booking.hasOne(Invoice, { foreignKey: 'bookingId' });
+Invoice.belongsTo(Booking, { foreignKey: 'bookingId' });
+
+User.hasMany(JobCard, { foreignKey: 'assignedTo' });
+JobCard.belongsTo(User, { foreignKey: 'assignedTo' });
+
+module.exports = {
+  sequelize,
+  User,
+  Customer,
+  Vehicle,
+  Booking,
+  Service,
+  Inventory,
+  JobCard,
+  Employee,
+  Invoice,
+  Message,
+};
